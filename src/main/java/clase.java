e9853b0fc2b1b9cc5edf8a09bca1de707f965bce
HuertasS <sebastianhuertasysique@gmail.com>
2025-04-01 14:11:17 -0500

    modifi3

--- src/main/java/clase.java
+++ src/main/java/clase.java
@@ -1,6 +1,8 @@
 public class clase {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo2");
+        System.out.println("Hola mundo3");
 
     }
 }
